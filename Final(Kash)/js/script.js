5661702b15538b3b65305fc2a707cf2e0c21982f
Akash Kumar <aka5hkumar@users.noreply.github.com>
2016-12-12 13:29:45 -0500

    finally

---
+++ Final(Kash)/js/script.js
@@ -0,0 +1,7 @@
+    $(".submitButton").click(function() {
+                (".Goal1").addClass("col-md-4");
+                (".Goal1").removeClass("col-md-6");
+                (".Goal2").addClass("col-md-4");
+                (".Goal2").removeClass("col-md-6");
+                (".Goal3").show();
+    });
