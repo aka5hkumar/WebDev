c6ec21563a6bea458e5e57615caafddbf3d5fd11
Akash Kumar <aka5hkumar@users.noreply.github.com>
2016-12-13 10:43:37 -0500

    Working JS

--- Final(Kash)/js/script.js
+++ Final(Kash)/js/script.js
@@ -1,7 +1,32 @@
-    $(".submitButton").click(function() {
-                (".Goal1").addClass("col-md-4");
-                (".Goal1").removeClass("col-md-6");
-                (".Goal2").addClass("col-md-4");
-                (".Goal2").removeClass("col-md-6");
-                (".Goal3").show();
-    });
+$(window).on('load', function() {
+
+    $("#Goal1_6").show();
+    $("#Goal2_6").show();
+    $("#Goal1_4").hide();
+    $("#Goal2_4").hide();
+    $("#Goal3").hide();
+    $('#startModal').modal('show');
+});
+
+$("#submitButton").click(function() {
+    $("#Goal1_6").hide();
+    $("#Goal2_6").hide();
+    $("#Goal1_4").show();
+    $("#Goal2_4").show();
+    $("#Goal3").show();
+    var priceVal = $('#price').val();
+    $('#cost3').text(priceVal);
+    $('#addButton').prop('disabled', true);
+});
+$('#product').blur(function() {
+    var objVal = $(this).val();
+    if (objVal != '') {
+        $('#name3_title').text(objVal);
+        $('#name3_modal').text(objVal);
+        $('#name3').text(objVal);
+        $('#cost').val('$2400');
+    }
+});
+$(function() {
+    $('#datepicker-12').datepicker();
+});
